bc721ca0851eb7746463391b8d9aed1860a0bbe7
vikas236 <saivias461@gmail.com>
2024-10-24 16:15:36 +0530

    add slider in gallery page

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -27,43 +27,82 @@ function gallery() {
 
   function showImages(data) {
     const gallery = document.querySelector(".main_gallery .gallery_images");
+    const slider = document.querySelector(".main_gallery .gallery_slider");
+    slider.classList.remove("active");
+    total_slides = data.length;
+    image_urls = data.slice();
 
     if (gallery.innerHTML === "") {
-      data.forEach((item) => {
-        createImage(item.image_data, gallery);
+      data.forEach((item, i) => {
+        createImage(item.image_data, gallery, "gallery", i);
       });
     }
   }
 
-  function createImage(url, container) {
+  function createImage(url, container, condition, i) {
     const image = document.createElement("img");
     image.src = url;
     container.appendChild(image);
+
+    if (condition === "gallery") {
+      image.addEventListener("click", () => {
+        slide_number = i + 1;
+        showImageSlider(image.src, i);
+      });
+    }
   }
 
+  let slide_number = 0;
+  let total_slides = 0;
+  let image_urls = [];
   function createimageSlider(data) {
     const slider = document.querySelector(".main_gallery .gallery_slider");
     const left_btn = document.createElement("div");
     const right_btn = document.createElement("div");
+    const close_btn = document.createElement("i");
 
     left_btn.classList.add("left_btn");
     right_btn.classList.add("right_btn");
+    close_btn.className = "bx bx-x close_btn";
 
     left_btn.innerHTML = "<i class='bx bx-chevron-left'></i>";
     right_btn.innerHTML = "<i class='bx bx-chevron-right'></i>";
 
+    slide_number = 1;
+    left_btn.addEventListener("click", (e) => moveSlider(e, "left"));
+    right_btn.addEventListener("click", (e) => moveSlider(e, "right"));
+    close_btn.addEventListener("click", closeSlider);
+
     if (slider.innerHTML === "") {
       slider.appendChild(left_btn);
       slider.appendChild(right_btn);
-      data.forEach((item, i) => {
-        createImage(item.image_data, slider);
-      });
+      slider.appendChild(close_btn);
+      createImage(data[0].image_data, slider);
     }
-    console.log(slider.childNodes);
   }
 
-  function showImageSlider(i) {
-    console.log(i);
+  function moveSlider(e, direction) {
+    let image = e.target.parentElement.parentElement.childNodes[3];
+    if (!image) image = e.target.parentElement.childNodes[3];
+    console.log(image);
+
+    if (direction === "left" && slide_number > 1) {
+      image.src = image_urls[--slide_number - 1].image_data;
+    } else if (direction === "right" && slide_number < total_slides) {
+      image.src = image_urls[++slide_number - 1].image_data;
+    }
+  }
+
+  function showImageSlider(url, i) {
+    const slider = document.querySelector(".gallery_slider");
+    slider.classList.add("active");
+    slider.childNodes[3].src = url;
+    slide_number = i + 1;
+  }
+
+  function closeSlider() {
+    const slider = document.querySelector(".main_gallery .gallery_slider");
+    slider.classList.remove("active");
   }
 
   return (
